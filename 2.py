9fe0f2403b30199d7a638d0b1dabc2cd79df6102
OleksandrXacKu <mystugins@gmail.com>
2024-09-20 11:45:38 +0300

    First

---
+++ 2.py
@@ -0,0 +1,19 @@
+#num = int(input("Введіть 4-х значне число: "))
+#q = num // 1000
+#w = (num % 1000) // 100
+#e = (num % 100) // 10
+#r = num % 10
+#print(q)
+#print(w)
+#print(e)
+#print(r)
+#...
+number = int(input("число: "))
+q, remainder = divmod(number, 1000)   # Отримуємо тисячі та залишок
+w, remainder = divmod(remainder, 100)  # Отримуємо сотні та залишок
+e, r = divmod(remainder, 10)  # Отримуємо десятки та одиниці
+print(q)
+print(w)
+print(e)
+print(r)
+...
