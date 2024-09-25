f43899d0e9ea4539bbd7f3d0c055757f1e7556d8
OleksandrXacKu <mystugins@gmail.com>
2024-09-25 12:13:42 +0300

    First

--- 4.py
+++ 4.py
@@ -48,3 +48,14 @@ else:
     result = [first_part, second_part]
 print(result)
 ...
+
+#def it1_list(lst):
+    #if len(lst) == 0:
+        #return [[], []]
+    #it2_list = (len(lst) +1) // 2
+    #return [lst[:it2_list], lst[it2_list:]]
+#print(it1_list([1, 2, 3, 4, 5, 6]))
+#print(it1_list([1, 2, 3]))
+#print(it1_list([1]))
+#print(it1_list([]))
+#...
