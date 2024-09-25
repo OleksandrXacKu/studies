2a7522de21f6b2a5f4ae3eafc71224bb5c5b4802
OleksandrXacKu <mystugins@gmail.com>
2024-09-25 12:00:30 +0300

    First

--- 6.py
+++ 6.py
@@ -11,4 +11,11 @@ elif operation == "*":
     result = number1 * number2
     print(f"результат: {number1} * {number2} = {result}")
 elif operation == "/":
-    result
+    if number2 == 0:
+        print("Error: делить на ноль нельзя!")
+    else:
+        result = number1 / number2
+        print(f"результат: {number1} / {number2} = {result}")
+else:
+    print("Error :")
+...
