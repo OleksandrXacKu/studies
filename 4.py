d55fe4e14d9c9e54c3f3fd5d17461280a1d6dd56
OleksandrXacKu <mystugins@gmail.com>
2024-09-25 10:17:40 +0300

    First

---
+++ 4.py
@@ -0,0 +1,9 @@
+lst = [1, 2, 3, 4, 5, 6]
+if len(lst) == 0:
+    result = [[], []]
+else:
+    mid = (len(lst) +1) // 2
+    first_part = lst[:mid]
+    second_part = lst[mid:]
+    result = [first_part, second_part]
+print(result)
