e95d0f683d0906d0e8e5af3ad77bb5816922f6ac
OleksandrXacKu <mystugins@gmail.com>
2024-09-25 10:41:20 +0300

    First

---
+++ 5.py
@@ -0,0 +1,18 @@
+lst = [12, 3, 4, 10]
+if len(lst) > 1:
+    lst = [lst[-1]] + lst[:-1]
+print(lst)
+...
+lst = [1]
+if len(lst) > 1:
+    lst = [lst[-1]] + lst[:-1]
+print(lst)
+...
+lst = []
+if len(lst) > 1:
+    lst = [lst[-1]] + lst[:-1]
+print(lst)
+...
+lst = [12, 3, 4, 10, 8]
+if len(lst) > 1:
+    lst = [lst[]]
