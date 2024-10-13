6ccf10a2fd11367864b8788db072f175d3967d21
OleksandrXacKu <mystugins@gmail.com>
2024-10-13 09:45:34 +0300

    First

--- 18.py
+++ 18.py
@@ -1,18 +1,31 @@
+# def correct_sentence(text: str) -> str:
+#     sentences = text.split(". ")
+#     corrected_sentences = []
+#     for sentence in sentences:
+#         sentence = sentence.strip()
+#         if sentence:
+#             sentence = sentence[0].upper() + sentence[1:]
+#             if not sentence.endswith("."):
+#                 sentence += "."
+#             corrected_sentences.append(sentence)
+#     return " ".join(corrected_sentences)
+# #Приклад :
+# assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+# assert correct_sentence("hello") == "Hello.", 'Test2'
+# assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+# assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+# assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+# print('ОК')
+# ...
+
 def correct_sentence(text: str) -> str:
     sentences = text.split(". ")
-    corrected_sentences = []
+    correct_sentences = []
     for sentence in sentences:
         sentence = sentence.strip()
         if sentence:
             sentence = sentence[0].upper() + sentence[1:]
             if not sentence.endswith("."):
-                sentence += "."
-            corrected_sentences.append(sentence)
-    return " ".join(corrected_sentences)
-#Приклад :
-assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
-assert correct_sentence("hello") == "Hello.", 'Test2'
-assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
-assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
-assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
-print('ОК')
+                sentence +="."
+            correct_sentences.append(sentence)
+    return " ".join(corr)
